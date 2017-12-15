6179d07eee60d891e501907f495c9b2f369b288c
Natalja <nata737mail@gmail.com>
2017-12-15 16:40:56 +0200

    Added c0 and c1

---
+++ c1.js
@@ -0,0 +1,8 @@
+function c1(x){
+    return (x-nodes[0])*(x-nodes[0])*(x-nodes[2])*(x-nodes[3])*(x-nodes[3])*(x-nodes[3])*K;
+}
+
+function K(){
+    return 1/((nodes[1]-nodes[0])*(nodes[1]-nodes[0])*(nodes[1]-nodes[2])*
+    (nodes[1]-nodes[3])*(nodes[1]-nodes[3])*(nodes[1]-nodes[3]));
+}
